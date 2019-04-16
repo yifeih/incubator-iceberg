422b81dc600c93a5834927e8e776b27389b5d7a2
Yifei Huang <yifeih@palantir.com>
2019-04-15 17:50:43 -0700

    dont wild card import

--- common/src/main/java/com/netflix/iceberg/common/DynFields.java
+++ common/src/main/java/com/netflix/iceberg/common/DynFields.java
@@ -19,7 +19,10 @@
 
 package com.netflix.iceberg.common;
 
-import com.google.common.base.*;
+import com.google.common.base.Joiner;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
 import com.google.common.collect.Sets;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
